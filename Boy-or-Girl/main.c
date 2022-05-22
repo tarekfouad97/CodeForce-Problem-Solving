c5da9ac91ebd34a82e6c14c48cc76eaec47edfcd
Tarek Fouad <tare2fo2ad@gmail.com>
2022-05-23 01:58:18 +0200

    fix bugs boy or girl

--- Boy-or-Girl/main.c
+++ Boy-or-Girl/main.c
@@ -1,4 +1,44 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
+int main()
+{
 
+    char str[100];
+    int count = 0;
+    int result = 0;
+    scanf("%s", str);
+
+    int size = 26;
+    int arr[size];
+    for (int i = 0; i <= size; i++)
+    {
+        arr[i] = 0;
+    }
+    int temp = 0;
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        temp = str[count] - 'a';
+        arr[temp]++;
+
+        count++;
+    }
+    for (int i = 0; i <= size; i++)
+    {
+        if (arr[i] >= 1)
+        {
+            result++;
+        }
+    }
+    
+    if (result % 2 == 0)
+    {
+        printf("CHAT WITH HER!");
+    }
+    else
+    {
+
+        printf("IGNORE HIM!");
+    }
+    return 0;
+}
